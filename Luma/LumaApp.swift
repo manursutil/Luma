10c706f59ce66093c36080b3f65746a1d3cc17ad
Manuel Rodríguez <mrodsut@gmail.com>
2026-06-13 12:31:18 +0100

    Initial Commit

---
+++ Luma/LumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumaApp.swift
+//  Luma
+//
+//  Created by Manuel Rodríguez Sutil on 13/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct LumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
